7d22fc2b29eab6f9b2a69c1c28821e6b60d1e55f
karan-suthar03 <116728069+karan-suthar03@users.noreply.github.com>
2026-02-13 16:48:08 +0530

    Simplify Build.bat by removing error check
    Removed error handling for the build process.

--- Build.bat
+++ Build.bat
@@ -5,9 +5,3 @@ if not exist out (
 )
 
 g++ -std=c++17 -g src\*.cpp -I include -o out\TextEditor.exe -lgdi32 -luser32 -DNDEBUG
-
-IF %ERRORLEVEL% NEQ 0 (
-    echo.
-    echo ❌ Build failed!
-    exit /b
-)
